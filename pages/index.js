da982637143dcebe9ed227ab9788452da320aa4e
Hussien2605 <168656898+Hussien2605@users.noreply.github.com>
2024-11-12 15:40:57 +0200

    Initial commit from Create Next App

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="bg-blue-900 w-screen min-h-screen flex items-center justify-center">
       <div>
-        <button className="bg-white p-2">login with google</button>
+        <button className="bg-white p-2">login with google or </button>
       </div>
     </div>
   );
