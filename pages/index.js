a8f4f9f8fd030b926e8dcfa6228270af036dca3e
Hussien2605 <168656898+Hussien2605@users.noreply.github.com>
2024-11-12 19:06:57 +0200

    admin authentication

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+
+
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen min-h-screen flex items-center justify-center">
-      <div>
-        <button className="bg-white p-2">login with google or </button>
-      </div>
-    </div>
-  );
+    const { data: session } = useSession()
+    if(!session) {
+        return (
+            <div className="bg-blue-900 w-screen min-h-screen flex items-center justify-center">
+                <div>
+                    <button onClick={() => signIn('google')} className="bg-white p-2 rounded-lg">login with google</button>
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div>logged in {session.user.email}</div>
+    )
+
 }
